c4cc1684b3f4889f7784aa3f3a06a36add121798
darshit <darshit.parkhiya@exo-field.com>
2020-07-03 19:04:34 +0530

    added materila tools

--- libs/theme/src/lib/theme.module.ts
+++ libs/theme/src/lib/theme.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 const UIComponents = [HeaderComponent];
 @NgModule({
+  imports: [
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   declarations: [HeaderComponent],
   exports: UIComponents,
 })
