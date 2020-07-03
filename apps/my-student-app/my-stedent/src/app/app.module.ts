c7604d4894be42305c7fd31b1f8993f57f2ecc8a
darshit <darshit.parkhiya@exo-field.com>
2020-07-03 18:52:28 +0530

    added angular theme package

--- apps/my-student-app/my-stedent/src/app/app.module.ts
+++ apps/my-student-app/my-stedent/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ThemeModule } from '@my-org/theme';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, ThemeModule],
+  imports: [BrowserModule, ThemeModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
