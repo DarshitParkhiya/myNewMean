696fa4f60243047af7cb59dcc971d1b4461baa94
darshit <darshit.parkhiya@exo-field.com>
2020-07-04 12:50:02 +0530

    added header and bootstrap

--- apps/my-student-app/my-stedent/src/app/app.module.ts
+++ apps/my-student-app/my-stedent/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ThemeModule } from '@my-org/theme';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, ThemeModule, BrowserAnimationsModule],
+  imports: [BrowserModule, ThemeModule, BrowserAnimationsModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
 })
